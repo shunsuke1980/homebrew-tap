8d78b1d166ed5473b767c8757fc87cf0e0fa53b3
ISHIKAWA Shunsuke <shunsuke1980@gmail.com>
2025-06-26 18:12:50 +0200

    Update detail info

--- Formula/flash_notify.rb
+++ Formula/flash_notify.rb
@@ -3,12 +3,37 @@ class FlashNotify < Formula
   homepage "https://github.com/shunsuke1980/flash_notify"
   url "https://github.com/shunsuke1980/flash_notify/archive/v1.0.0.tar.gz"
   sha256 "d9db34d83e3028e021d5a164b95c23d9704622a1493db5fc9f8b1f149357c93d"
-  
+  license "MIT"
+  version "1.0.0"
+
+  depends_on :macos
+
   def install
     bin.install "flash_notify"
   end
   
   test do
-    system "#{bin}/flash_notify", "--version"
+    # Test version output
+    assert_match "flash_notify version #{version}", shell_output("#{bin}/flash_notify --version")
+
+    # Test help output
+    assert_match "Flash macOS menu bar notification tool", shell_output("#{bin}/flash_notify --help")
+
+    # Test that the script exists and is executable
+    assert_predicate bin/"flash_notify", :exist?
+    assert_predicate bin/"flash_notify", :executable?
+  end
+
+  def caveats
+    <<~EOS
+      flash_notify requires permission to control System Events.
+
+      On first run, macOS may prompt you to grant permission.
+      If flash_notify doesn't work, check System Preferences:
+        Security & Privacy > Privacy > Accessibility
+
+      Make sure your terminal application has permission to control System Events.
+    EOS
   end
 end
+
